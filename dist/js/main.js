ab920bf12f2ae4ffe01337c4080255986b1eb412
viki korso <viki.korso@gmail.com>
2020-02-06 19:16:11 +0200

    viki 1

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,146 @@
-// progressbar.js@1.0.0 version is used
-// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+// tabs
 
-var bar = new ProgressBar.Circle(container, {
-    strokeWidth: 6,
-    easing: 'easeInOut',
-    color: '#12E193',
-    trailColor: '#646883',
-    trailWidth: 1,
-    svgStyle: null
+// $(".acc-head").click(function () {
+
+//     if ($("#open-tab").is(":hidden")) {
+
+//         $("#open-tab").slideDown("slow");
+//         $(".tab").addClass("open");
+
+//     } else {
+
+//         $("#open-tab").hide("slow");
+//         $(".tab").removeClass("open");
+
+//     }
+// });
+
+$(document).ready(function () {
+    //прикрепляем клик по заголовкам acc-head
+    $('#accordeon .acordion-head').on('click', f_acc);
 });
 
-bar.animate(0.68, {
-    duration: 800
-}); // Number from 0.0 to 1.0
+function f_acc() {
+    //скрываем все кроме того, что должны открыть
+    $('#accordeon .acordion-body').not($(this).next()).slideUp(1000);
+    $('div .acordion-head').toggleClass('.acordion-head-change');
+    // открываем или скрываем блок под заголовком, по которому кликнули
+    $(this).next().slideToggle(1000);
+}
+
+// swiper
+
+let swiper = new Swiper('.swiper-container', {
+    spaceBetween: -450,
+    // loop: true,
+    centeredSlides: true,
+    // autoplay: {
+    //     delay: 2000,
+    //     disableOnInteraction: false,
+    // },
+});
+
+// progres-bar
+
+(function ($) {
+
+    $.fn.bekeyProgressbar = function (options) {
+
+        options = $.extend({
+            animate: true,
+            animateText: true
+        }, options);
+
+        let $this = $(this);
+
+        let $progressBar = $this;
+        let $progressCount = $progressBar.find('.ProgressBar-percentage--count');
+        let $circle = $progressBar.find('.ProgressBar-circle');
+        let percentageProgress = $progressBar.attr('data-progress');
+        let percentageRemaining = (100 - percentageProgress);
+        let percentageText = $progressCount.parent().attr('data-progress');
+
+        //Calcule la circonférence du cercle
+        let radius = $circle.attr('r');
+        let diameter = radius * 2;
+        let circumference = Math.round(Math.PI * diameter);
+
+        //Calcule le pourcentage d'avancement
+        let percentage = circumference * percentageRemaining / 100;
+
+        $circle.css({
+            'stroke-dasharray': circumference,
+            'stroke-dashoffset': percentage
+        })
+
+        //Animation de la barre de progression
+        if (options.animate === true) {
+            $circle.css({
+                'stroke-dashoffset': circumference
+            }).animate({
+                'stroke-dashoffset': percentage
+            }, 3000)
+        }
+
+        //Animation du texte (pourcentage)
+        if (options.animateText == true) {
+
+            $({
+                Counter: 0
+            }).animate({
+                Counter: percentageText
+            }, {
+                duration: 3000,
+                step: function () {
+                    $progressCount.text(Math.ceil(this.Counter) + '%');
+                }
+            });
+
+        } else {
+            $progressCount.text(percentageText + '%');
+        }
+
+    };
+
+})(jQuery);
+
+$(document).ready(function () {
+
+    $('.ProgressBar--animateNone').bekeyProgressbar({
+        animate: false,
+        animateText: false
+    });
+
+    $('.ProgressBar--animateCircle').bekeyProgressbar({
+        animate: true,
+        animateText: false
+    });
+
+    $('.ProgressBar--animateText').bekeyProgressbar({
+        animate: false,
+        animateText: true
+    });
+
+    $('.ProgressBar--animateAll').bekeyProgressbar();
+
+});
+
+// modal
+
+let modal = document.getElementById("my_modal");
+let btn = document.getElementById("btn_modal_window");
+let span = document.getElementsByClassName("close_modal_window")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
